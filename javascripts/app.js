43c3627f41379ef17a8e037b6a5f26a704f418f0
Francisco Vaca <fcovacarh@gmail.com>
2019-03-15 02:55:21 +0100

    Added obstacles

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,27 +6,63 @@ var initialPosition = {
   y: 0,
 };
 
+var scale = 50;
+
 var rover = {
   direction: "N",
   position: initialPosition,
   travelLog: [],
 };
 
+var obstacles = [];
+
+initObstaclesArray(10);
 updateDisplay();
 
+function initObstaclesArray(number){
+  for(var i = 0; i < number; i++){
+    var x = Math.floor(Math.random() * 10) + 1;
+    var y = Math.floor(Math.random() * 10) + 1;
+
+    obstacles.push([x, y]);
+  }
+}
+
 function drawRover(){
-  ctx.clearRect(0,0, 500, 500);    
+  ctx.clearRect(0,0, scale * 10, scale * 10);    
   ctx.fillStyle = "#fff";
   ctx.fillRect(
-    rover.position.x*50, 
-    rover.position.y*50,
-    50,
-    50
+    rover.position.x*scale, 
+    rover.position.y*scale,
+    scale,
+    scale
   );
+}
+
+function drawObstacles(){
+  obstacles.forEach(obstacle => {
+    ctx.fillStyle = "#ff0000";
+    ctx.fillRect(
+      obstacle[0]*scale,
+      obstacle[1]*scale,
+      scale,
+      scale
+    )
+  });
+}
+
+function isNextPositionAvailable(nextPosition){
+  for(var obstacle of obstacles){
+    if(nextPosition.x == obstacle[0] && nextPosition.y == obstacle[1]){
+      return false;
+    }
+  };
+  return true;
 }
 
 function updateDisplay(){
   drawRover();
+  drawObstacles();
   updateTravelLog(rover);
 }
 
@@ -94,35 +130,35 @@ function turnRight(rover){
 
 function moveForward(rover){
   console.log("moveForward was called");
+  var nextPosition = {
+    x: rover.position.x, 
+    y: rover.position.y,
+  };
   switch (rover.direction) {
     case "N":
       if(rover.position.y > 0){
-        rover.position.y--;
-        updateDisplay(rover);
+        nextPosition.y = (rover.position.y - 1);
       } else {
         console.log("Rover can't move in that direction");
       }
       break;
     case "W":
       if(rover.position.x > 0){
-        rover.position.x--;
-        updateDisplay(rover);
+        nextPosition.x = (rover.position.x - 1);
       } else {
         console.log("Rover can't move in that direction");
       }
       break;
     case "S":
       if(rover.position.y < 9){
-        rover.position.y++;
-        updateDisplay(rover);
+        nextPosition.y = (rover.position.y + 1);
       } else {
         console.log("Rover can't move in that direction");
       }
       break;
     case "E":
       if(rover.position.x < 9){
-        rover.position.x++;
-        updateDisplay(rover);
+        nextPosition.x = (rover.position.x + 1);
       } else {
         console.log("Rover can't move in that direction");
       }
@@ -130,7 +166,15 @@ function moveForward(rover){
     default:
       break;
   }
-  console.log("position (X:" + rover.position.x + ", Y:" + rover.position.y + ")");
+
+  if(isNextPositionAvailable(nextPosition)){
+    rover.position.x = nextPosition.x;
+    rover.position.y = nextPosition.y;
+  } else {
+    console.log("Rover can't move in that direction");
+  }
+
+  updateDisplay(rover);
 }
 
 function moveBackward(rover){
